f203512eea4e4101d598ca6efd62cf83c8546403
RabbaniHub <rabbanishaik0708@gmail.com>
2024-11-05 16:41:16 +0530

    Add project files.

---
+++ ToDoApi/Data/ToDoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using ToDoApi.Model;
+
+namespace ToDoApi.Data
+{
+    public class ToDoDbContext : DbContext
+    {
+        public ToDoDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
---
+++ ToDoApi/Model/Todo.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ToDoApi.Model
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime? CompletedDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedDate { get; set; }
+    }
+}
